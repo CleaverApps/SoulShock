b84d7b4c063adc84e35b2d54a3ad3427f75cc1a0
CleaverApps <Benjamin@Benjamins-MacBook-Pro.local>
2015-05-11 01:14:35 -0400

    SoulShock
    Music App

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoulShock
+//
+//  Created by Benjamin VanCleave on 4/15/15.
+//  Copyright (c) 2015 Cleaver Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoulShock
+//
+//  Created by Benjamin VanCleave on 4/15/15.
+//  Copyright (c) 2015 Cleaver Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
